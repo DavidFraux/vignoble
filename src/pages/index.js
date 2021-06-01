86e5846a8a9ca7f0e4f0268b6e898cc3f3387147
mquantin <matthieu@tinta.fr>
2021-06-01 17:26:50 +0200

    new index layout according to Ludivine template

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,39 +2,45 @@ import React from 'react';
 //import Image from "../components/image.js"
 import { navigate } from "gatsby"
 import {
-  link,
-  pictlink,
-  overlay,
+  acces,
+  accesPict,
+  accesText,
+  accesSubText,
   background,
   logo,
   pub,
   container,
   pasApas,
   film,
-  explorer, } from './index.module.css'
-import filmPict from '../images/film.png'
-import explorerPict from '../images/explorer.png'
-import pasApasPict from '../images/pasApas.png'
+  explorer, 
+  savoirPlus} from './index.module.css'
+import filmPict from '../images/logoFilm.svg'
+import explorerPict from '../images/logo3D.svg'
+import pasApasPict from '../images/logopasApas.svg'
 import logoPict from '../images/LOGO-MUSEE.png'
 
-const ShapeLink = props => (
-    <div role = 'button' tabIndex = {0} className = {`${props.classe} ${link}`} onClick={() => navigate(props.to)} >
-        <img  className = {pictlink} src={props.picture} alt={props.text} />
+
+
+const Acces = props => (
+    <div role = 'button' tabIndex = {0} className = {`${props.classe} ${acces}`} onClick={() => navigate(props.to)} >
+        <img className = {accesPict} src={props.picture} alt={props.text} />
+        <div className = {accesText}> {props.text} </div>
+        <div className = {accesSubText}> {props.subtext} </div>
   </div>
 )
 
 function Home() {
   return (
     <React.Fragment>
-    <div className = {overlay}/>
     <div className = {background}/>
     <img  className = {logo} src={logoPict} alt='logo musée' />
-    <p className = {pub}>Le pressoir long-fut <br/> 1848</p>
+    <p className = {pub}>COMPRENDRE LES PRESSOIRS</p>
     <div className = {container}>
-      <ShapeLink to = '/pasApas/' classe = {pasApas}  text = "pasApas" picture = {pasApasPict} />
-      <ShapeLink to = '/film/' classe = {film} text = "film"  picture = {filmPict}/>
-      <ShapeLink to = '/explorer/' classe ={explorer} text = "explorer"  picture = {explorerPict}/>
+      <Acces to = '/pasApas/' classe = {pasApas}  text = "Pas à pas" subtext = 'Le fonctionnement du long-fût' picture = {pasApasPict} />
+      <Acces to = '/film/' classe = {film} text = "Voir le film"  subtext = "Un pressurage avec un long-fût" picture = {filmPict}/>
+      <Acces to = '/explorer/' classe ={explorer} text = "Explorer en 3D"  subtext = 'Les annecdotes du long-fût' picture = {explorerPict}/>
     </div>
+    <button className = {savoirPlus}             onClick={() => navigate('/savoirPlus')}>En savoir plus</button>
     
     </React.Fragment>
   )
