1332603b059a9e78dcefc25ae04c1351ffa0766c
mquantin <matthieu@tinta.fr>
2021-06-07 19:57:19 +0200

    analogClock animated to show time duration instead of clock time

--- src/components/analogClock.js
+++ src/components/analogClock.js
@@ -8,42 +8,50 @@ class AnalogClock extends React.Component {
 	constructor(props) {
 		super(props);
     //#TODO simplify it -> not a class is needed
-    //#TODO get duration of step to animate that svg clock instead of displaying the clock time
-		const now = new Date();
-    const s = now.getSeconds();
-    const m = now.getMinutes();
-    const h = now.getHours();
-
-		this.state = {
-      //"180 +" because the svg rect is drawn toward the bottom, so 12h00 is shown 6h30 on the analog clock
-			angleSeconds: 180 + 360 * s / 60,
-			angleMinutes: 180 + 360 * m / 60,
-			angleHours:   180 + 360 * (h % 12) / 12  +  360 * m / 60 / 12,
-		};
+    console.log(props.from);
+    const fromTime = props.from.split(':');//should get an array [hh, mm]
+    const toTime = props.to.split(':');//should get an array [hh, mm]
+    const fromMin = fromTime[1];
+    const fromHour = fromTime[0];
+    const toMin = toTime[1];
+    const toHour = toTime[0];
+    this.fromAngleHour = 180 + 360 * fromHour / 12  +  360 * fromMin / 60 / 12;
+    console.log(this.fromAngleHour);
+    this.toAngleHour = 180 + 360 * toHour / 12  +   360 * toMin / 60 / 12;
+    this.fromAngleMin = 180 + 360 * fromMin / 60;
+    this.toAngleMin = 180 + 360 * toMin / 60 + 360 * (toHour-fromHour);
 	}
+
+  buildFace(){
+    const centre = <circle key= "centre" cx="50" cy="50" r="3" fill="black" />;
+    const tour = <circle key= "tour" cx="50" cy="50" r="49" fill="rgba(0,0,0,0)" strokeWidth="0.2" stroke= "black" opacity="1"/>;
+    const fontSize = 12;
+    const numero6 =  <text key= "numero6" transform="translate(45 95)" fontSize={fontSize} >6</text>;
+    const numero3 =  <text key= "numero3" transform="translate(90 55)" fontSize={fontSize}>3</text>;
+    const numero9 =  <text key= "numero9" transform="translate(5 55)" fontSize={fontSize}>9</text>;
+    const numero12 = <text key= "numero12" transform="translate(43 15)" fontSize={fontSize}>12</text>;
+    let face = [centre, tour, numero3, numero6, numero9, numero12];
+    const exceptions = [12, 3, 6, 9];
+    for (let i= 1; i<=12; i++){
+      if(exceptions.includes(i)){continue;}
+      let deg = 360/12*i;
+      face.push(<rect key= {`trait ${i}`} transform={`rotate(${deg},50,50)`} x="50" y="2" width="1" height="3" opacity=".4" fill="black"/>)
+    };
+    return face
+  }
 
 
 
 	render() {
-    const testDate = new Date('2021-01-01T15:32:21');
-    console.log(testDate);
 		return (
 			<div className={clock}>
         <svg viewBox="0 0 100 100">
         <defs>
-            <clipPath id="seconds">
-              <rect x="49.5" y="50" width="1" height="45" >
-                <animateTransform 
-                  attributeName="transform" attributeType="XML"
-                  type="rotate" from={`${this.state.angleSeconds} 50,50`} to={`${this.state.angleSeconds+360} 50,50`} dur={`${60}s`} repeatCount="indefinite">
-                </animateTransform>
-              </rect>
-          </clipPath>
           <clipPath id="minutes">
             <rect x="49" y="50" width="2" height="42" >
                 <animateTransform 
                   attributeName="transform" attributeType="XML" 
-                  type="rotate" from={`${this.state.angleMinutes} 50,50`} to={`${this.state.angleMinutes+360} 50,50`} dur={`${60*60}s`}  repeatCount="indefinite">
+                  type="rotate" from={`${this.fromAngleMin} 50,50`} to={`${this.toAngleMin} 50,50`} dur={`10s`} fill="freeze" >
                 </animateTransform>				
             </rect>
           </clipPath>
@@ -51,19 +59,22 @@ class AnalogClock extends React.Component {
             <rect x="48" y="50" width="4" height="27" >
               <animateTransform 
                 attributeName="transform" attributeType="XML" 
-                type="rotate" from={`${this.state.angleHours} 50,50`} to={`${this.state.angleHours+360} 50,50`} dur={`${60*60*12}s`} repeatCount="indefinite">
+                type="rotate" from={`${this.fromAngleHour} 50,50`} to={`${this.toAngleHour} 50,50`} dur={`10s`} fill="freeze" >
               </animateTransform>				
             </rect>
           </clipPath>
         </defs>
           
         <g>
-          <rect id="sec" width="100" height="100" fill="red" opacity=".4" clipPath="url(#seconds)" />
           <rect id="min"  width="100" height="100" fill="black" opacity=".4" clipPath="url(#minutes)" />
           <rect id="hour"  width="100" height="100" fill="black" opacity=".4" clipPath="url(#hours)" />
         </g>
-        <circle cx="50" cy="50" r="3" style={{"fill":"rgba(0,0,0,.94)"}} />
-        <circle cx="50" cy="50" r="49" fill="rgba(0,0,0,0)" style={{"stroke":"rgba(0,0,0,.94)"}} />
+        {this.buildFace()}
+        
+        {/* <circle cx="50" cy="50" r="3" style={{"fill":"rgba(0,0,0,.94)"}} />
+        <circle cx="50" cy="50" r="49" fill="rgba(0,0,0,0)" style={{"stroke":"rgba(0,0,0,.94)"}} /> */}
+
+        
         </svg>
 			</div>
 		)
