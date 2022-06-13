a7acd2c5bd9fc199ae5172e2220712eca69909a7
mquantin <matthieu@tinta.fr>
2022-06-13 18:34:42 +0200

    credit page with logo

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -9,13 +9,15 @@ import {
   affiliation,
   task,
   logo } from './credits.module.css';
+import logoECN from '../images/Centrale_Nantes_Logo.svg'
 import fetchAPI from '../components/fetchREST.js';
 
 const Contributor = props => (
   <tr>
     {/* <td className = {affiliation} >{props.affiliation}</td> */}
-    <td className= {logo} > <img src={process.env.GATSBY_API_URL + props.logo.url} alt={props.affiliation} /> </td>
+    
     <td className = {noun} >{props.noun}</td>
+     <td className= {logo} > {props.logo && <img src={process.env.GATSBY_API_URL + props.logo.url} alt={props.affiliation} />} </td>
     <td className = {task}>{props.task}</td>
   </tr>
   )
@@ -26,6 +28,7 @@ function Credit() {
     fetchAPI('creditedpeople')
     .then(data => {
         const contributors = [];
+        data.map(a => {a.appearOrder ??= 999} );//if no appearOrder is specified, it's at the bottom
         data.sort((a, b) => a.appearOrder - b.appearOrder);
         for (const contributor of data){
           contributors.push(<Contributor key={contributor.id} {...contributor}/>);
@@ -50,7 +53,9 @@ function Credit() {
           {/* ME, HARD CODED */}
           <tr key="matthieuQuantin">
             {/* <td className = {affiliation} >Laboratoire LS2N, Nantes Université, École Centrale Nantes</td> */}
+            
             <td className = {noun} >Matthieu Quantin</td>
+            <td className={logo}> <img src={logoECN} alt="logo ECN"/></td>
             <td className = {task}>Développement de l'application et modélisation 3D</td>
           </tr>
           {/* OTHER PEOPLE CREDITED FROM API DATA */}
